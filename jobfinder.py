7706e711fff34db0e3bbaf0530f35343b8e5e202
Nick Piegari <nickpiegari@gmail.com>
2022-12-12 14:37:53 -0500

    modification

--- jobfinder.py
+++ jobfinder.py
@@ -1,11 +1,11 @@
 # !/usr/bin/python
 
-# path = "/Volumes/LaCie/Ingest/"
-path = "/Volumes/nickpiegari/Desktop/dv concatenation test/"
+path = "/Volumes/LaCie/Ingest/"
+# path = "/Volumes/nickpiegari/Desktop/dv concatenation test/"
 
 import os, csv, ffmpeg
 
-videoformats = ["mov", "avi", "mkv"]
+videoformats = ["dv"]
 
 with open("videolist.csv", "w") as csvfile:
 	rofl = csv.writer(csvfile)
@@ -16,13 +16,14 @@ with open("videolist.csv", "w") as csvfile:
 			filesize = os.path.getsize(filepath)
 
 			try:
-				if name.rsplit(".")[1] in videoformats:
+				if name.rsplit(".")[1] in videoformats and filepath.find(".Spotlight") == -1 and filepath.find(".Trashes") == -1:
 					rofl.writerow([filepath.replace(path, ""), filesize, "transcode", "pending", ""])
+					print (filepath)
 			except IndexError:
 				pass
 			# rofl.writerow([filepath, filesize])
-		for name in dirs:
-			print(os.path.join(root, name))
+		# for name in dirs:
+			# print(os.path.join(root, name))
    
 			
 
